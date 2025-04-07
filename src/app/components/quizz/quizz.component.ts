68b768e5f6f1b87e1b2307ae25900b1e2dd6cbe1
Silas Sefas <silassefas23@gmail.com>
2025-04-07 16:13:08 -0300

    Criando a Logica por traz do teste

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,11 +1,79 @@
-import { Component } from '@angular/core';
+import { NgIf, NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import quizz_question from "../../../assets/data/quizz_questions.json"
 
 @Component({
   selector: 'app-quizz',
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
+
+  title:string = ""
+
+  questions:any
+  questionSelected:any
+
+  answers:string[] = []
+  answerSelected:string =""
+
+  questionIndex:number =0
+  questionMaxIndex:number=0
+
+  finished:boolean = false
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if(quizz_question){
+      this.finished = false
+      this.title = quizz_question.title
+
+      this.questions = quizz_question.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+
+      console.log(this.questionIndex)
+      console.log(this.questionMaxIndex)
+    }
+
+  }
+
+  playerChoose(value:string){
+    this.answers.push(value)
+    this.nextStep()
+
+  }
+
+  async nextStep(){
+    this.questionIndex+=1
+
+    if(this.questionMaxIndex > this.questionIndex){
+        this.questionSelected = this.questions[this.questionIndex]
+    }else{
+      const finalAnswer:string = await this.checkResult(this.answers)
+      this.finished = true
+      this.answerSelected = quizz_question.results[finalAnswer as keyof typeof quizz_question.results ]
+    }
+  }
+
+  async checkResult(anwsers:string[]){
+
+    const result = anwsers.reduce((previous, current, i, arr)=>{
+        if(
+          arr.filter(item => item === previous).length >
+          arr.filter(item => item === current).length
+        ){
+          return previous
+        }else{
+          return current
+        }
+    })
+
+    return result
+  }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { QuizzComponent } from '../../components/quizz/quizz.component'; // Ajuste o caminho conforme necessário
 
 @Component({
@@ -7,6 +7,11 @@ import { QuizzComponent } from '../../components/quizz/quizz.component'; // Ajus
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
